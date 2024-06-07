268efeafc3079a87f6cef6418af877b56e8abbf6
WebFan37 <118756551+WebFan37@users.noreply.github.com>
2024-06-07 15:01:41 +0000

    Un peu de redesign pour header dans CSS et Typescript. l'ajout dans App.tsx pour le visualiser.

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,62 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> */}
+
+            <section className="header1">
+                <h1>JANNE'S PORTFOLIO</h1>
+                <p>Welcome to my portfolio</p>
+
+                <section className="infoHeader1">
+                    <input type="checkbox" id="check" />
+                    <label htmlFor="check" className="clicking-bar">
+                        <span className="material-symbols-outlined">menu</span>
+                    </label>
+                    <div className="menu">
+                        <div>
+                        <span className="material-symbols-outlined">home</span>
+                            <a href="main-menu.php">HOME </a>
+                        </div>
+                      
+
+                        <div>
+                            <span className="material-symbols-outlined">image</span>
+                            
+                            <a href="top-picked-photos.php">PHOTOS</a>
+                        </div>
+
+                        <div>
+                            <span className="material-symbols-outlined">videocam</span>
+                          
+                            <a href="videos.php"> VIDEOS</a>
+                        </div>
+
+                        <div>
+                            <span className="material-symbols-outlined">public</span>
+                            
+                            <a href="web.php">WEB</a>
+                        </div>
+
+                        <div>
+                            <span className="material-symbols-outlined">contacts</span>
+                                
+                                <a href="contact.php">CONTACT</a>
+                        </div>
+                        
+                        <div>
+                            <span className="material-symbols-outlined">brush</span>
+                            <a href="graphic-design.php">GRAPHIC DESIGN</a>
+                        </div>
+
+                        <div>
+                            <span className="material-symbols-outlined">music_note</span>
+                            <a href="contact.php">MUSIC</a>
+                        </div>
+                    </div>
+              </section>
+
+            </section>
+    
+            
       
       <main>
 
--- src/common-pages/header.tsx
+++ src/common-pages/header.tsx
@@ -3,57 +3,58 @@ import './header.css'
 export default function header(){
     <>
       
-            <section className="header1">
+      <section className="header1">
                 <h1>JANNE'S PORTFOLIO</h1>
                 <p>Welcome to my portfolio</p>
-            </section>
-    
-            <section className="header2">
-                <input type="checkbox" id="check" />
-                <label htmlFor="check" className="clicking-bar">
-                    <span className="material-symbols-outlined">menu</span>
-                </label>
-                <div className="menu">
-                    <div>
-                    <span className="material-symbols-outlined">home</span>
-                        <a href="main-menu.php">HOME </a>
-                    </div>
-                   
 
-                    <div>
-                        <span className="material-symbols-outlined">image</span>
-                        
-                        <a href="top-picked-photos.php">PHOTOS</a>
-                    </div>
+                <section className="infoHeader1">
+                    <input type="checkbox" id="check" />
+                    <label htmlFor="check" className="clicking-bar">
+                        <span className="material-symbols-outlined">menu</span>
+                    </label>
+                    <div className="menu">
+                        <div>
+                        <span className="material-symbols-outlined">home</span>
+                            <a href="main-menu.php">HOME </a>
+                        </div>
+                      
 
-                    <div>
-                        <span className="material-symbols-outlined">videocam</span>
-                       
-                        <a href="videos.php"> VIDEOS</a>
-                    </div>
-
-                    <div>
-                        <span className="material-symbols-outlined">public</span>
-                        
-                        <a href="web.php">WEB</a>
-                    </div>
-
-                    <div>
-                        <span className="material-symbols-outlined">contacts</span>
+                        <div>
+                            <span className="material-symbols-outlined">image</span>
                             
-                            <a href="contact.php">CONTACT</a>
-                    </div>
-                    
-                    <div>
-                        <span className="material-symbols-outlined">brush</span>
-                        <a href="graphic-design.php">GRAPHIC DESIGN</a>
-                    </div>
+                            <a href="top-picked-photos.php">PHOTOS</a>
+                        </div>
 
-                    <div>
-                        <span className="material-symbols-outlined">music_note</span>
-                        <a href="contact.php">MUSIC</a>
+                        <div>
+                            <span className="material-symbols-outlined">videocam</span>
+                          
+                            <a href="videos.php"> VIDEOS</a>
+                        </div>
+
+                        <div>
+                            <span className="material-symbols-outlined">public</span>
+                            
+                            <a href="web.php">WEB</a>
+                        </div>
+
+                        <div>
+                            <span className="material-symbols-outlined">contacts</span>
+                                
+                                <a href="contact.php">CONTACT</a>
+                        </div>
+                        
+                        <div>
+                            <span className="material-symbols-outlined">brush</span>
+                            <a href="graphic-design.php">GRAPHIC DESIGN</a>
+                        </div>
+
+                        <div>
+                            <span className="material-symbols-outlined">music_note</span>
+                            <a href="contact.php">MUSIC</a>
+                        </div>
                     </div>
-                </div>
+              </section>
+
             </section>
     </>
 }
